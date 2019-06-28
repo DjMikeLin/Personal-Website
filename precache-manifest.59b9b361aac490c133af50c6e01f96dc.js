2521c5eab1eaa455d3b5810ce86d698c53b77d74
DjMikeLin <linmichael97@yahoo.com>
2019-06-28 17:13:54 -0400

    Updates

---
+++ precache-manifest.59b9b361aac490c133af50c6e01f96dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53126fb9044de366efec84a3737af397",
+    "url": "/profolio/index.html"
+  },
+  {
+    "revision": "fc65b1d39a7d0280570e",
+    "url": "/profolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/profolio/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "fc65b1d39a7d0280570e",
+    "url": "/profolio/static/js/main.e0128dc3.chunk.js"
+  },
+  {
+    "revision": "f73b13ae382e930697e1",
+    "url": "/profolio/static/js/runtime~main.70384152.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/profolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
