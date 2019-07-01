1ae101509f22a57c3240b6fe9579308690549701
DjMikeLin <linmichael97@yahoo.com>
2019-07-01 16:16:25 -0400

    1. Removed redundancies created by create-react-app 2. Updated Projects to sort the repos by creation date

--- profolio/src/components/Projects.js
+++ profolio/src/components/Projects.js
@@ -6,17 +6,23 @@ class Projects extends React.Component{
     state = {
         projects: []
     }
-
+    //When component loads update state, get all repos from github and sort then by creation date
     componentDidMount = async() => {
-        this.setState({projects: (await allProjects()).data});
+        let repos = (await allProjects()).data;
+        repos.sort(function(a,b){
+            return new Date(b.created_at) - new Date(a.created_at);
+        });
+        this.setState({projects: repos});
     }
 
     render(){
         return(
-            <div>{
-                this.state.projects.map((element, index) => {
-                    return <Project key={index} project={element} />; 
-                })}
+            <div>
+                {
+                    this.state.projects.map((element, index) => {
+                        return <Project key={index} project={element} />; 
+                    })
+                }
             </div>
         )
     }
